cd031b9cbefcde4bd217127e442bcf4892d74240
Ray Lucas <Ray.Lucas@team2342.org>
2016-06-04 15:39:11 -0400

    Fixed favicon.ico loading

--- index.js
+++ index.js
@@ -6,16 +6,21 @@ var fs = require("fs");
 var express = require("express");
 
 var app = express();
-
-app.use(express.static('./public'));
-
+/*
 app.get("/", function(request, result){
 	result.write("<!DOCTYPE html> <html>Hello...<br>This is line 2</html>");
 	result.end();
 });
+*/
+app.use(express.static('./public'));
 
 app.get("/favicon.ico", function(req, res){
 	fs.readFile("./public/favicon.ico", function(err, content){
+		res.statusCode = 200;
+		res.setHeader('Content-Length', content.length);
+		res.setHeader('Content-Type', 'image/x-icon');
+		res.setHeader("Cache-Control", "public, max-age=25");
+		//res.setHeader("Expires", new Date(Date.now() + 2592000000).toUTCString());
 		res.end(content);
 	});
 });
